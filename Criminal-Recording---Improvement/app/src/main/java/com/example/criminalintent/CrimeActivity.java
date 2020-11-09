fffcdc8317dea61cf94eba71f9ab62d53dcc2c63
S-TJones <jones.shemar.sj@gmail.com>
2020-11-08 21:53:35 -0500

    Edited CrimeFragment class
    Added a LayoutInflater, it also handles the receiving of data the user entered and adding them to the Crime class using the setters defined in the Crime class. The CrimeFragment xml file was tweaked a bit as well.

--- Criminal-Recording---Improvement/app/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ Criminal-Recording---Improvement/app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -9,7 +9,7 @@ public class CrimeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.fragment_crime);
     }
 
 }
